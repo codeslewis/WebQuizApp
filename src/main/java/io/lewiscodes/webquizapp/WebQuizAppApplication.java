ebb392a1bd1681c0271424817253417bf31a9853
Lewis Sneddon <lewis@lewiscodes.io>
2021-07-07 09:58:09 +0100

    Initial commit

---
+++ src/main/java/io/lewiscodes/webquizapp/WebQuizAppApplication.java
@@ -0,0 +1,13 @@
+package io.lewiscodes.webquizapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebQuizAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebQuizAppApplication.class, args);
+    }
+
+}
