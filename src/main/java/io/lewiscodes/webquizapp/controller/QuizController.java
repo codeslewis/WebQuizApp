91f8341f409c7cf128a30a2337adac338ae94ad7
Lewis Sneddon <lewis@lewiscodes.io>
2021-07-07 10:28:03 +0100

    REST Controller to solve questions

--- src/main/java/io/lewiscodes/webquizapp/controller/QuizController.java
+++ src/main/java/io/lewiscodes/webquizapp/controller/QuizController.java
@@ -1,14 +1,19 @@
 package io.lewiscodes.webquizapp.controller;
 
+import io.lewiscodes.webquizapp.model.Feedback;
 import io.lewiscodes.webquizapp.model.Question;
 import io.lewiscodes.webquizapp.service.Quiz;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Collection;
 
 /*
-    REST Controller to create, get by id, get all and solve by id
+    REST Controller for Quiz
+        CREATE new Question
+        GET All Questions
+        GET Question by ID
+        Solve Question by ID and request param answer
  */
 
 @RestController
@@ -20,9 +25,28 @@ public class QuizController {
         this.quiz = quiz;
     }
 
-    // Create a question and add to quiz
     @PostMapping
     public Question create(@RequestBody Question newQuestion) {
         return quiz.create(newQuestion);
+    }
+
+    @GetMapping
+    public Collection<Question> getAllQuestions() {
+        return quiz.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Question> getById(@PathVariable("id") int id) {
+        Question question = quiz.find(id);
+        return question != null ? ResponseEntity.ok(question) : ResponseEntity.notFound().build();
+    }
+
+    @PostMapping("/{id}/solve")
+    public ResponseEntity<Feedback> solve(@PathVariable("id") int id, @RequestParam("answer") int answer) {
+        Question question = quiz.find(id);
+        if (question == null) {
+            return ResponseEntity.notFound().build();
+        }
+        return question.getAnswer() == answer ? ResponseEntity.ok(Feedback.CORRECT) : ResponseEntity.ok(Feedback.WRONG);
     }
 }
--- src/main/java/io/lewiscodes/webquizapp/model/Feedback.java
+++ src/main/java/io/lewiscodes/webquizapp/model/Feedback.java
@@ -1,7 +1,7 @@
 package io.lewiscodes.webquizapp.model;
 
 public class Feedback {
-    public static final Feedback RIGHT = new Feedback(true, "Congratulations, you're right!");
+    public static final Feedback CORRECT = new Feedback(true, "Congratulations, you're right!");
     public static final Feedback WRONG = new Feedback(false, "Wrong answer! Please, try again.");
 
     private boolean success;
@@ -16,15 +16,7 @@ public class Feedback {
         return success;
     }
 
-    public void setSuccess(boolean success) {
-        this.success = success;
-    }
-
     public String getFeedback() {
         return feedback;
-    }
-
-    public void setFeedback(String feedback) {
-        this.feedback = feedback;
     }
 }
